effe7c5d5972670d3e35852da5ebf21a772513e6
Robby Xtab <55150346+xtab6@users.noreply.github.com>
2026-05-14 23:48:01 +0700

    Update GenerativeModel to use gemini-1.5-flash

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
