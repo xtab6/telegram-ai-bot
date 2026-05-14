577b3f9ae85f8835a490961525b1922aee555a19
Robby Xtab <55150346+xtab6@users.noreply.github.com>
2026-05-14 23:35:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
+
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
+    try:
+        text = update.message.text
 
-    response = model.generate_content(text)
+        response = model.generate_content(text)
 
-    await update.message.reply_text(response.text)
+        await update.message.reply_text(response.text)
+
+    except Exception as e:
+        print("ERROR:", e)
+        await update.message.reply_text("Terjadi error.")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, reply)
+)
 
 print("Bot running...")
 
